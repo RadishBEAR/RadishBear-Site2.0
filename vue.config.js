8c14408576dc95f7d26bbf5b65ee1fe8614f4fca
RadishBear <1798164846@qq.com>
2021-07-18 11:10:09 +0800

    fix:修改了ci文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/vue-running" : "/",
+    outputDir: 'dist',
+    publicPath: process.env.NODE_ENV === "production" ? "/radish-bear-site" : "/",
     publicPath: "./",
 }
